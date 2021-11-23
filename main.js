d1a240770d9a3931900070aa8af595ba4707f011
KayLemba <kaylemba@yahoo.com>
2021-11-23 18:29:29 +0200

    Add local storage class

--- main.js
+++ main.js
@@ -37,3 +37,52 @@ class Displays {
       document.querySelector('#author').value = '';
   }
 }
+
+class Storage {
+  static getBooks() {
+      let books;
+      if (localStorage.getItem('books') === null) {
+          books = [];
+      } else {
+          books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
+  }
+
+  static addBook(book) {
+      const books = Storage.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(author) {
+      const books = Storage.getBooks();
+       books.forEach ((book,index) => {
+          if (book.author === author) {
+             
+              books.splice(index, 1);
+           }
+       });
+       localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', Displays.displayBooks);
+document.querySelector('#book-form').addEventListener('submit', (e) => 
+{
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const book = new Book(title,author);
+
+  Displays.addBookToList(book);
+  Storage.addBook(book);
+  Displays.clearFields();
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) =>
+{
+  Displays.deleteBook(e.target);
+
+  Storage.removeBook(e.target.parentElement.previousSibling.previousSibling.textContent);
+});
