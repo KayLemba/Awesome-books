1da24d9bac19a0f98cd6ea61003c438b31a85c1c
Mong'are <Mongaresams@gmail.com>
2021-11-23 19:13:48 +0300

    Change the main.js to use class

--- main.js
+++ main.js
@@ -1,53 +1,39 @@
-const bookList = document.querySelector('.books');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const addBook = document.querySelector('#add-books');
-
-const Book = function theBook(title, author) {
-  this.title = title;
-  this.author = author;
-};
-
-let storedData = [];
-
-function addBookToList(newBook) {
-  const bookTemp = `
-    <div class="book">
-      <h2>Title: ${newBook.title}</h2>
-      <h2>Author: ${newBook.author}</h2>
-      <button class="delete" type="button">Remove</button>
-      <hr>
-    </div>
-  `;
-  document.querySelector('.books').innerHTML += bookTemp;
-  return document.querySelector('.books').innerHTML;
+class Book {
+  constructor (title,author) {
+      this.title = title;
+      this.author = author;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (bookList !== null) {
-    storedData = [...JSON.parse(localStorage.getItem('localBookList'))];
-    storedData.forEach((item) => {
-      addBookToList(item);
-    });
+class Displays {
+  static displayBooks() {
+      const books = Storage.getBooks();
+  books.forEach((book) => Displays.addBookToList(book));
   }
-});
 
-addBook.addEventListener('click', (e) => {
-  const newBook = new Book(title.value, author.value);
-  e.preventDefault();
-  addBookToList(newBook);
-  storedData.push(newBook);
-  title.value = '';
-  author.value = '';
-  localStorage.setItem('localBookList', JSON.stringify(storedData));
-});
+  static addBookToList(book) {
+      const list = document.querySelector('#book-list');
 
-bookList.addEventListener('click', (el) => {
-  if (el.target.classList.contains('delete')) {
-    document.querySelector('.books').removeChild(el.target.parentElement);
-    const bookD = el.target.parentElement;
-    const removeBook = storedData.find((item) => item.title === bookD.firstChild.innerText);
-    storedData.splice(storedData.indexOf(removeBook), 1);
-    localStorage.setItem('localBookList', JSON.stringify(storedData));
+      const texty = document.createElement('h4');
+
+      texty.innerHTML = `
+      <h4>${book.title}</h4>
+      <h4>${book.author}</h4>
+      <h4><button type="button" class= "delete">Remove </button></h4>
+      <hr>
+      `;
+
+      list.appendChild(texty);
   }
-});
+
+  static deleteBook(el) {
+      if (el.classList.contains ('delete')) {
+          el.parentElement.parentElement.remove();
+      }
+  }
+
+  static clearFields() {
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
+  }
+}
