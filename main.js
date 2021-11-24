a7e50340f6edac12db02404285b8bb76b5272513
Mong'are <Mongaresams@gmail.com>
2021-11-24 09:10:53 +0300

    Fix linters error

--- main.js
+++ main.js
@@ -1,88 +1,80 @@
-class Book {
-  constructor (title,author) {
-      this.title = title;
-      this.author = author;
-  }
-}
-
 class Displays {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Displays.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(author) {
+    const books = Displays.getBooks();
+    books.forEach((book, index) => {
+      if (book.author === author) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
   static displayBooks() {
-      const books = Storage.getBooks();
-  books.forEach((book) => Displays.addBookToList(book));
+    const books = Displays.getBooks();
+    books.forEach((book) => Displays.addBookToList(book));
   }
 
   static addBookToList(book) {
-      const list = document.querySelector('#book-list');
+    const list = document.querySelector('#book-list');
 
-      const texty = document.createElement('h4');
+    const texty = document.createElement('h4');
 
-      texty.innerHTML = `
+    texty.innerHTML = `
       <h4>${book.title}</h4>
       <h4>${book.author}</h4>
       <h4><button type="button" class= "delete">Remove </button></h4>
       <hr>
       `;
 
-      list.appendChild(texty);
+    list.appendChild(texty);
   }
 
   static deleteBook(el) {
-      if (el.classList.contains ('delete')) {
-          el.parentElement.parentElement.remove();
-      }
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
   }
 
   static clearFields() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
-  }
-}
-
-class Storage {
-  static getBooks() {
-      let books;
-      if (localStorage.getItem('books') === null) {
-          books = [];
-      } else {
-          books = JSON.parse(localStorage.getItem('books'));
-      }
-      return books;
-  }
-
-  static addBook(book) {
-      const books = Storage.getBooks();
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBook(author) {
-      const books = Storage.getBooks();
-       books.forEach ((book,index) => {
-          if (book.author === author) {
-             
-              books.splice(index, 1);
-           }
-       });
-       localStorage.setItem('books', JSON.stringify(books));
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 }
 
 document.addEventListener('DOMContentLoaded', Displays.displayBooks);
-document.querySelector('#book-form').addEventListener('submit', (e) => 
-{
+document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
-  const book = new Book(title,author);
+  function Book(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  const book = new Book(title, author);
 
   Displays.addBookToList(book);
-  Storage.addBook(book);
+  Displays.addBook(book);
   Displays.clearFields();
 });
 
-document.querySelector('#book-list').addEventListener('click', (e) =>
-{
+document.querySelector('#book-list').addEventListener('click', (e) => {
   Displays.deleteBook(e.target);
 
-  Storage.removeBook(e.target.parentElement.previousSibling.previousSibling.textContent);
+  Displays.removeBook(e.target.parentElement.previousSibling.previousSibling.textContent);
 });
